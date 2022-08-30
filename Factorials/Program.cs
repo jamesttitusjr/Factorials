99de121a59e18834470cca228421ce6c36adaab7
James Titus <jamesttitusjr@outlook.com>
2022-08-30 11:06:39 -0400

    Add project files.

---
+++ Factorials/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Factorial!");
+
+// Calculate 6 factorial (6!)
+static int Factorial (6)
+{
+    if (number < 1)
+    {
+        return 0;
+    }
+    else if (number == 1)
+    { 
+        return 1;
+    }
+    else
+    {
+        return 6 * Factorial(number - 1);
+    }
+
+}
+
